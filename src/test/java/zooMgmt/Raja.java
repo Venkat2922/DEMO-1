a576a3cd335dbba4c4c8dfa9781cb4b524815ce7
Venkat2922 <tungavenkat146@gmail.com>
2023-01-12 17:24:49 +0530

    rr

---
+++ src/test/java/zooMgmt/Raja.java
@@ -0,0 +1,11 @@
+package zooMgmt;
+
+public class Raja {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("good");
+
+	}
+
+}
---
+++ src/test/java/zooMgmt/Rddd.java
@@ -0,0 +1,10 @@
+package zooMgmt;
+
+public class Rddd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hai");
+	}
+
+}
